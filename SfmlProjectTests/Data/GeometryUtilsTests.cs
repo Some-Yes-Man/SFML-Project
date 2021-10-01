f6fbd5f93fb1a8502b1b677125bf35e0742c8d9e
Yes-Man <yes-man@web.de>
2021-10-01 21:29:26 +0200

    rename

---
+++ SfmlProjectTests/Data/GeometryUtilsTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SFMLTest.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SFMLTest.Data.Tests {
+    [TestClass()]
+    public class GeometryUtilsTests {
+        [TestMethod()]
+        public void PointOnLineTest() {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void PointInTriangleTest() {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void PointInTriangleTest1() {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void PointInCircleTest() {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void LineIntersectsTriangleTest() {
+            Assert.Fail();
+        }
+    }
+}
